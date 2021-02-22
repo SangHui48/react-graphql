e6eecdd5c8970338cd7cbb8f359f066324a3df23
전병길 <marr1144@naver.com>
2021-02-22 22:45:58 +0900

    Product 추가 Form 수정

--- src/components/products/products-form/products-form.component.tsx
+++ src/components/products/products-form/products-form.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 import { useCreateProduct } from '../../../hooks/products/useCreateProducts';
 
 interface FormData {
@@ -10,10 +11,14 @@ interface FormData {
 const ProductsForm: React.FC = () => {
     const { register, handleSubmit } = useForm<FormData>();
     const createProduct = useCreateProduct();
-
+    const history = useHistory();
     const onSubmit = handleSubmit(({ name, quantity }) => {
         createProduct({ variables: { name: name, quantity: quantity } });
+        setTimeout(() => {
+            history.push('/');
+        }, 1000)
     })
+
     return (
         <div className="products-form">
             <form onSubmit={onSubmit}>
